1b5d8e6139758d8f748d6950d0f7c177acf2f18e
CSapien08 <sapiano08@gmail.com>
2021-08-09 02:57:59 -0700

    finished week 5

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,37 +17,124 @@
 
 // a) Create a test with an expect statement using the variables provided.
 
-var secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-var secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-var secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+//Create a test function.
+//  use describe() to pass in the name of the function, messageCoder
+//  use it() to describe what messageCoder is doing
+//      within it() write the test using expect and toEquals
+//      test to make sure it returns the expected output
+//END
 
+describe("messageCoder", () => {
+    var secretCodeWord1 = "Lackadaisical"
+    // Expected output: "L4ck4d41s1c4l"
+    var secretCodeWord2 = "Gobbledygook"
+    // Expected output: "G0bbl3dyg00k"
+    var secretCodeWord3 = "Eccentric"
+    // Expected output: "3cc3ntr1c"
 
+    it("Takes in a string and returns a coded message.", () => {
+        expect(messageCoder(secretCodeWord1)).toEqual('L4ck4d41s1c4l')
+        expect(messageCoder(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(messageCoder(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
 
 // b) Create the function that makes the test pass.
+//Create a function called messageCoder
+//  parameter - string
+//  turn the string into array using spilt()
+//  use map to iterate over the string of arrays
+//      if the value is equal 'a' convert it to a 4,
+//          or if it is equal to 'e' convert it to a 3,
+//          or if it is equal to 'i' convert it to a 1
+//          or if it is equal to 'o' convert it to a 0
+//  return the string array joined back as a string using join
+//END
 
+const messageCoder = (string) => {
+    return string.split('').map(value => {
+        if(value.toLowerCase() === 'a' ){
+            return value = 4
+        }
+        else if(value.toLowerCase() === 'e'){
+            return value = 3
+        }
+        else if(value.toLowerCase() === 'i'){
+            return value = 1
+        }
+        else if(value.toLowerCase() === 'o'){
+            return value = 0
+        }
+        else {
+            return value
+        }
+    }).join('')
+}
 
+//console.log(messageCoder(secretCodeWord1));
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
 
 // a) Create a test with an expect statement using the variable provided.
 
-var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
-// Expected output: ["Apple", "Banana", "Orange"]
-var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
-// Expected output: ["Mango", "Apricot", "Peach"]
+//Create a test function.
+//  use describe() to pass in the name of the function, findWord
+//  use it() to describe what findWord is doing
+//      within it() write the test using expect and toEquals
+//      test to make sure it returns the expected output
+//END
 
+describe("findWord", () => {
+    var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+    // Expected output: ["Apple", "Banana", "Orange"]
+    var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+    // Expected output: ["Mango", "Apricot", "Peach"]
 
+    it("takes in an array and returns all the words that contain the letter a", () => {
+        expect(findWord(arrayOfWords1)).toEqual(["Apple", "Banana", "Orange"])
+        expect(findWord(arrayOfWords2)).toEqual(["Mango", "Apricot", "Peach"])
+    })
+})
 
 // b) Create the function that makes the test pass.
+//Create a function called findWords
+//  parameters - array of words
+//  use filter to iterate the array and find all of the words that contain the letter 'a'
+//  return the words that contain the letter 'a'
+//END
 
-
+const findWord = (words) => {  
+    return words.filter(value => {
+        if(value.toLowerCase().includes('a')){
+            return value
+        }
+    })
+}
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with an expect statement using the variable provided.
+//Create a test function.
+//  use describe() to pass in the name of the function, findWord
+//  use it() to describe what findWord is doing
+//      within it() write the test using expect and toEquals
+//      test to make sure it returns the expected output
+//END
+
+describe("isFullHouse", () => {
+    var hand1 = [5, 5, 5, 3, 3]
+    // Expected output: true
+    var hand2 = [5, 5, 3, 3, 4]
+    // Expected output: false
+    var hand3 = [5, 5, 5, 5, 4]
+    // Expected output: false
+
+    it("takes in an array of 5 numbers and determines whether or not the array is a “full house”.", () => {
+        expect(isFullHouse(hand1)).toEqual(true)
+        expect(isFullHouse(hand2)).toEqual(false)
+        expect(isFullHouse(hand3)).toEqual(false)
+    })
+})
 
 var hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -56,6 +143,28 @@ var hand2 = [5, 5, 3, 3, 4]
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
-
-
 // b) Create the function that makes the test pass.
+
+//Create a function called isFullHouse
+//  parameters - array of 5 numbers
+//  create an array of 13 0's to represent the 13 cards count
+//  iterate over the hand and whatever the value of the card is add 1 to the count for that value in the count array
+//  A full house should have 3 and 2 of the same values so check to make there is a count of 3 and a count of 2
+//  if true return true else return false
+//END
+
+const isFullHouse = (hand) => {
+    handCountArr = Array(13).fill(0);
+   
+    for(let i = 0; i < hand.length; i++){
+        handCountArr[hand[i]-1] += 1 
+    }
+    if(handCountArr.includes(3) && handCountArr.includes(2)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+//console.log(isFullHouse(hand1));
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Rails follows an MVC pattern. What does that mean?
 
-  Your answer:
+  Your answer: MVC stands for model, view, control.
 
-  Researched answer:
+  Researched answer: Rails likes to separte the idea of an applications data, user interface and control logic. They have seperate files to handle each part of the pattern.
 
 
 
 2. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: Relational databases are databases that have aceess to data that is related to them.
 
-  Researched answer:
+  Researched answer: Relational databases are collections of data and items with a pre-defined relationship between them.
 
 
 
 3. What is object-relational mapping?
 
-  Your answer:
+  Your answer: Not really sure.
 
-  Researched answer:
+  Researched answer: Object-relational mapping is a way to convert data between two incompatible type systems using OOP languages. It appears to take the data from a database using SQL and converts into arrays or vice versa.
 
 
 
 4. What is a gem?
 
-  Your answer:
+  Your answer: A gem is a dependency in ruby.
 
-  Researched answer:
+  Researched answer: A gem is a module or library that can be installed on any project that you create.
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: Primary keys are unique identifers for a relational database.
 
-  Researched answer:
+  Researched answer: A primary key is a special databse table column that uniquely identifies or ties it to another table.
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: RESTful routing is a set of standards used in many different languages to create efficient, reusable routes. It aims to map HTTP methods (GET, POST, PATCH, DELETE) and CRUD actions.
 
-2. JSON:
+2. JSON: JavaScript Object Notation is a text format for storing and transporting data using key and value pairs
 
-3. ERB:
+3. ERB: Embeds Ruby into HTML
 
-4. Params:
+4. Params: Refers to the parameters being passed to the controller from a GET or POST request.
 
-5. API:
+5. API: Application Programming Interface is an interface that allows two different applications to talk to each other. 
